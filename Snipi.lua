08a1dd854a7fa607578c25c17fc7cf4c1ed82d85
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-02-21 16:45:08 +0100

    Update Snipi.lua

--- Snipi.lua
+++ Snipi.lua
@@ -31,7 +31,7 @@ getgenv().Settings = {
             ["Golden Titanic Loveserker"] = {Price = "2%"},
             ["Titanic Loveserker"] = {Price = "1%"},
             ["All Huges"] = {Price = "5%", AllTypes = true},
-            ["Love Gift"] = {Price = "2%"},
+            ["Love Gift"] = {Price = "4%"},
             ["Huge Machine Butterfly Egg"] = {Price = "1%"},
         },
         ["Switch Servers"] = {Active = true, MinuteDelay = 20, OnlyPRO = false},
