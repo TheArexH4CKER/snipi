d87d8bd46addd41964f096f01299cef855d02920
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-02-24 13:36:15 +0100

    Update Snipi.lua

--- Snipi.lua
+++ Snipi.lua
@@ -28,7 +28,7 @@ getgenv().Settings = {
     Seller = {
         Active = true,
         ["Items"] = {
-            ["Golden Titanic Loveserker"] = {Price = "8%"},
+            ["Golden Titanic Loveserker"] = {Price = "10%"},
             ["Retro Pack"] = {Price = "15%"},   
             ["Fantasy Pack"] = {Price = "6%"},   
             ["Bucket O' Magic"] = {Price = "2%"},   
@@ -37,7 +37,7 @@ getgenv().Settings = {
             ["Clan Gift"] = {Price = "6%"},   
             ["Snowflake Gift"] = {Price = "6%"},   
             ["Hellfire Gift"] = {Price = "6%"},   
-            ["All Huges"] = {Price = "10%", AllTypes = true},
+            ["All Huges"] = {Price = "12%", AllTypes = true},
             ["Love Gift"] = {Price = "6%"},
             ["Huge Machine Butterfly Egg"] = {Price = "1%"},
         },
