62d4a14143ffce8da20a18c256ba0ba3615fbd89
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-03-23 10:42:47 +0100

    Update Snipi.lua

--- Snipi.lua
+++ Snipi.lua
@@ -29,7 +29,7 @@ getgenv().Settings = {
         Active = true,
         ["Items"] = {
             ["Golden Titanic Bobcat"] = {Price = 26000000000},    
-            ["All Huges"] = {Price = "1%", AllTypes = true},
+            ["All Huges"] = {Price = "2%", AllTypes = true},
             ["Gargantuan Machine Grim Reaper Egg"] = {Price = "1%"},
             ["Diamond Orb"] = {Price = "1%"},
             ["Bucket O' Magic"] = {Price = "2%"},   
