ed6e7ed1414ff975542f40463e93e78e9477bdef
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-03-24 10:40:06 +0100

    Update Snipi.lua

--- Snipi.lua
+++ Snipi.lua
@@ -28,7 +28,7 @@ getgenv().Settings = {
     Seller = {
         Active = true,
         ["Items"] = {
-            ["Golden Titanic Bobcat"] = {Price = 26000000000},    
+            ["Golden Titanic Bobcat"] = {Price = 23500000000},    
             ["All Huges"] = {Price = "2%", AllTypes = true},
             ["Gargantuan Machine Grim Reaper Egg"] = {Price = "1%"},
             ["Diamond Orb"] = {Price = "1%"},
